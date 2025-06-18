e50a8560d0363a6fb2b46d0a5e4e35d8fea846da
Ayush Srivastava <srivastavaaayush73@gmail>
2025-06-18 13:31:10 +0530

    assignment 1 complete

---
+++ tuteDude classes/assignment-1/Task 1.py
@@ -0,0 +1,14 @@
+# Take two numbers as input from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+# Perform basic operations
+addition = num1 + num2
+subtraction = num1 - num2
+multiplication = num1 * num2
+division = num1 / num2
+
+# Display the results of each operation on the screen.
+print("Addition:", addition)
+print("Subtraction:", subtraction)
+print("Multiplication:", multiplication)
+print("Division:", division)
---
+++ tuteDude classes/assignment-1/Task 2.py
@@ -0,0 +1,9 @@
+# Takes a user's first and last name as input.
+first_name= str(input("Enter your first name: "))
+last_name = str(input("Enter your last name: "))
+
+# Concatenate first and last name to create a full name
+full_name = first_name + " " + last_name 
+
+# print a personalized greeting message using the full name
+print("\n" "Hello, " + full_name + "! Welcome to the python program.")

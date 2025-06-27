996851438a057ce17812c34b7c9fde1b58acf8db
Ayush Srivastava <srivastavaaayush73@gmail>
2025-06-27 12:18:30 +0530

    assigement-3

--- tuteDude classes/assigement-3/Task_1.py
+++ tuteDude classes/assigement-3/Task_1.py
@@ -2,7 +2,7 @@ def factorial(n):
     result = 1
     for i in range(1, n+1):
         result = i * result
-    return result 
+    return result    
 
 number = 5
 print("Factorial of", number, "is :", factorial(number))

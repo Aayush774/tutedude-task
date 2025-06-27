0330db89c0f6d2c4542568fa56cf02cb3b0af077
Ayush Srivastava <srivastavaaayush73@gmail>
2025-06-27 11:48:30 +0530

    assignment 3

---
+++ tuteDude classes/assigement-3/Task_1.py
@@ -0,0 +1,8 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n+1):
+        result = i * result
+    return result
+
+number = 5
+print("Factorial of", number, "is :", factorial(number))
---
+++ tuteDude classes/assigement-3/Task_2.py
@@ -0,0 +1,10 @@
+import math
+num = int(input("Enter a number:"))
+
+square_root = math.sqrt(num)
+nalural_log = math.log(num)
+sine_value = math.sin(num)
+print("Square root:", square_root)
+print("Logarithm:", nalural_log)
+print("Sine:", sine_value)
+

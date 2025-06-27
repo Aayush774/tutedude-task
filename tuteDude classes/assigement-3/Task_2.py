07523b39354147afaa6f2f6a1a24a647abb714ca
Ayush Srivastava <srivastavaaayush73@gmail>
2025-06-27 11:51:00 +0530

    assignment-3

--- tuteDude classes/assigement-3/Task_2.py
+++ tuteDude classes/assigement-3/Task_2.py
@@ -6,5 +6,5 @@ nalural_log = math.log(num)
 sine_value = math.sin(num)
 print("Square root:", square_root)
 print("Logarithm:", nalural_log)
-print("Sine:", sine_value)
+print("Sine:", sine_value) 
 
